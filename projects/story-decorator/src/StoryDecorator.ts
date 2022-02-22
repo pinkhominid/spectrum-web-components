zzuzsj/iliad-ui

feat: support express flavor of Spectrum

projects/story-decorator/src/StoryDecorator.ts
@@ -22,6 +22,7 @@ import {
 } from '@spectrum-web-components/base';
 import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
+import '@spectrum-web-components/theme/src/express/themes.js';
 import '@spectrum-web-components/field-label/sp-field-label.js';
 import '@spectrum-web-components/picker/sp-picker.js';
 import '@spectrum-web-components/menu/sp-menu.js';
@@ -29,7 +30,7 @@ import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/switch/sp-switch.js';
 import { Picker } from '@spectrum-web-components/picker';
 import { Switch } from '@spectrum-web-components/switch';
-import { Scale, Color, Theme } from '@spectrum-web-components/theme';
+import { Scale, Color, Theme, Flavor } from '@spectrum-web-components/theme';
 import { ActiveOverlay } from '@spectrum-web-components/overlay';
 import './types.js';
 
@@ -38,11 +39,13 @@ const urlParams = new URLSearchParams(queryString);
 
 export let dir: 'ltr' | 'rtl' =
     (urlParams.get('sp_dir') as 'ltr' | 'rtl') || 'ltr';
+export let theme: Flavor = (urlParams.get('sp_theme') as Flavor) || 'classic';
 export let color: Color = (urlParams.get('sp_color') as Color) || 'light';
 export let scale: Scale = (urlParams.get('sp_scale') as Scale) || 'medium';
 export let reduceMotion = urlParams.get('sp_reduceMotion') === 'true';
 
 window.__swc_hack_knobs__ = window.__swc_hack_knobs__ || {
+    defaultFlavor: theme,
     defaultColor: color,
     defaultScale: scale,
     defaultDirection: dir,
@@ -68,7 +71,7 @@ const reduceMotionProperties = css`
 ActiveOverlay.prototype.renderTheme = function (
     content: TemplateResult
 ): TemplateResult {
-    const { color, scale, lang } = this.theme;
+    const { color, scale, theme, lang } = this.theme;
     return html`
         ${window.__swc_hack_knobs__.defaultReduceMotion
             ? html`
@@ -80,6 +83,7 @@ ActiveOverlay.prototype.renderTheme = function (
               `
             : html``}
         <sp-theme
+            theme=${ifDefined(theme)}
             color=${ifDefined(color)}
             scale=${ifDefined(scale)}
             lang=${ifDefined(lang)}
@@ -91,73 +95,78 @@ ActiveOverlay.prototype.renderTheme = function (
 };
 
 export class StoryDecorator extends SpectrumElement {
-    static styles = [
-        css`
-            sp-theme {
-                overflow-x: hidden;
-                display: block;
-                box-sizing: border-box;
-                width: 100%;
-                min-height: 100vh;
-                padding: var(--spectrum-global-dimension-size-100)
-                    var(--spectrum-global-dimension-size-100)
-                    calc(
-                        2 * var(--spectrum-alias-focus-ring-size) +
-                            var(--spectrum-alias-item-height-m)
+    static get styles() {
+        return [
+            css`
+                sp-theme {
+                    overflow-x: hidden;
+                    display: block;
+                    box-sizing: border-box;
+                    width: 100%;
+                    min-height: 100vh;
+                    padding: var(--spectrum-global-dimension-size-100)
+                        var(--spectrum-global-dimension-size-100)
+                        calc(
+                            2 * var(--spectrum-alias-focus-ring-size) +
+                                var(--spectrum-alias-item-height-m)
+                        );
+                    box-sizing: border-box;
+                    background-color: var(--spectrum-global-color-gray-100);
+                    color: var(
+                        --spectrum-body-text-color,
+                        var(--spectrum-alias-text-color)
                     );
-                box-sizing: border-box;
-                background-color: var(--spectrum-global-color-gray-100);
-                color: var(
-                    --spectrum-body-text-color,
-                    var(--spectrum-alias-text-color)
-                );
-            }
-            :host([screenshot]) sp-theme {
-                padding: var(--spectrum-global-dimension-size-100);
-            }
-            :host([reduce-motion]) sp-theme {
-                ${reduceMotionProperties}
-            }
-            .manage-theme {
-                position: fixed;
-                bottom: 0;
-                left: var(--spectrum-global-dimension-size-200);
-                right: var(--spectrum-global-dimension-size-200);
-                display: flex;
-                align-items: center;
-                justify-content: flex-end;
-                box-sizing: border-box;
-                background-color: var(--spectrum-global-color-gray-100);
-                padding-bottom: calc(2 * var(--spectrum-alias-focus-ring-size));
-            }
-            [dir='ltr'] sp-field-label {
-                padding-left: 0;
-                padding-right: var(
-                    --spectrum-fieldlabel-side-padding-x,
-                    var(--spectrum-global-dimension-size-100)
-                );
-                margin-left: var(--spectrum-global-dimension-size-400);
-            }
-            [dir='ltr'] sp-switch {
-                margin-left: var(--spectrum-global-dimension-size-400);
-                margin-right: 0;
-                padding: 0;
-            }
-            [dir='rtl'] sp-field-label {
-                padding-right: 0;
-                padding-left: var(
-                    --spectrum-fieldlabel-side-padding-x,
-                    var(--spectrum-global-dimension-size-100)
-                );
-                margin-right: var(--spectrum-global-dimension-size-400);
-            }
-            [dir='rtl'] sp-switch {
-                margin-right: var(--spectrum-global-dimension-size-400);
-                margin-left: 0;
-                padding: 0;
-            }
-        `,
-    ];
+                }
+                :host([screenshot]) sp-theme {
+                    padding: var(--spectrum-global-dimension-size-100);
+                }
+                :host([reduce-motion]) sp-theme {
+                    ${reduceMotionProperties}
+                }
+                .manage-theme {
+                    position: fixed;
+                    bottom: 0;
+                    left: var(--spectrum-global-dimension-size-200);
+                    right: var(--spectrum-global-dimension-size-200);
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-end;
+                    box-sizing: border-box;
+                    background-color: var(--spectrum-global-color-gray-100);
+                    padding-bottom: calc(2 * var(--spectrum-alias-focus-ring-size));
+                }
+                [dir='ltr'] sp-field-label {
+                    padding-left: 0;
+                    padding-right: var(
+                        --spectrum-fieldlabel-side-padding-x,
+                        var(--spectrum-global-dimension-size-100)
+                    );
+                    margin-left: var(--spectrum-global-dimension-size-400);
+                }
+                [dir='ltr'] sp-switch {
+                    margin-left: var(--spectrum-global-dimension-size-400);
+                    margin-right: 0;
+                    padding: 0;
+                }
+                [dir='rtl'] sp-field-label {
+                    padding-right: 0;
+                    padding-left: var(
+                        --spectrum-fieldlabel-side-padding-x,
+                        var(--spectrum-global-dimension-size-100)
+                    );
+                    margin-right: var(--spectrum-global-dimension-size-400);
+                }
+                [dir='rtl'] sp-switch {
+                    margin-right: var(--spectrum-global-dimension-size-400);
+                    margin-left: 0;
+                    padding: 0;
+                }
+            `,
+        ];
+    }
+
+    @property({ type: String })
+    public theme: Flavor = window.__swc_hack_knobs__.defaultFlavor;
 
     @property({ type: String })
     public color: Color = window.__swc_hack_knobs__.defaultColor;
@@ -176,16 +185,16 @@ export class StoryDecorator extends SpectrumElement {
     public screenshot = false;
 
     @queryAsync('sp-theme')
-    private theme!: Theme;
+    private themeRoot!: Theme;
 
     public ready = false;
 
     public async startManagingContentDirection(el: HTMLElement): Promise<void> {
-        (await this.theme).startManagingContentDirection(el);
+        (await this.themeRoot).startManagingContentDirection(el);
     }
 
     public async stopManagingContentDirection(el: HTMLElement): Promise<void> {
-        (await this.theme).stopManagingContentDirection(el);
+        (await this.themeRoot).stopManagingContentDirection(el);
     }
 
     private updateTheme({ target }: Event & { target: Picker | Switch }): void {
@@ -193,6 +202,9 @@ export class StoryDecorator extends SpectrumElement {
         const { value } = target as Picker;
         const { checked } = target as Switch;
         switch (id) {
+            case 'theme':
+                this.theme = theme = window.__swc_hack_knobs__.defaultFlavor = value as Flavor;
+                break;
             case 'color':
                 this.color = color = window.__swc_hack_knobs__.defaultColor = value as Color;
                 break;
@@ -218,11 +230,22 @@ export class StoryDecorator extends SpectrumElement {
         }
     }
 
+    private get themedColor(): Color {
+        return (this.color +
+            (this.theme === 'express' ? '-express' : '')) as Color;
+    }
+
+    private get themedScale(): Scale {
+        return (this.scale +
+            (this.theme === 'express' ? '-express' : '')) as Scale;
+    }
+
     protected render(): TemplateResult {
         return html`
             <sp-theme
-                color=${this.color}
-                scale=${this.scale}
+                theme=${this.theme}
+                color=${this.themedColor}
+                scale=${this.themedScale}
                 dir=${this.direction}
                 part="container"
                 @keydown=${this.handleKeydown}
@@ -258,9 +281,25 @@ export class StoryDecorator extends SpectrumElement {
     private get manageTheme(): TemplateResult {
         return html`
             <div class="manage-theme" part="controls">
-                ${this.colorControl} ${this.scaleControl} ${this.dirControl}
-                ${this.reduceMotionControl}
+                ${this.themeControl} ${this.colorControl} ${this.scaleControl}
+                ${this.dirControl} ${this.reduceMotionControl}
             </div>
+        `;
+    }
+
+    private get themeControl(): TemplateResult {
+        return html`
+            <sp-field-label for="theme">Spectrum</sp-field-label>
+            <sp-picker
+                id="theme"
+                placement="top"
+                quiet
+                .value=${this.theme}
+                @change=${this.updateTheme}
+            >
+                <sp-menu-item value="classic">Classic</sp-menu-item>
+                <sp-menu-item value="express">Express</sp-menu-item>
+            </sp-picker>
         `;
     }
 

pinkhominid/spectrum-web-components

projects/story-decorator/src/StoryDecorator.ts
@@ -24,6 +24,7 @@ import {
 import { ifDefined } from '@spectrum-web-components/base/src/directives.js';
 import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
+import '@spectrum-web-components/theme/src/express/themes.js';
 import '@spectrum-web-components/field-label/sp-field-label.js';
 import '@spectrum-web-components/picker/sp-picker.js';
 import '@spectrum-web-components/menu/sp-menu.js';
@@ -31,7 +32,7 @@ import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/switch/sp-switch.js';
 import { Picker } from '@spectrum-web-components/picker';
 import { Switch } from '@spectrum-web-components/switch';
-import { Color, Scale, Theme } from '@spectrum-web-components/theme';
+import { Color, Flavor, Scale, Theme } from '@spectrum-web-components/theme';
 import { ActiveOverlay } from '@spectrum-web-components/overlay';
 import './types.js';
 
@@ -40,11 +41,13 @@ const urlParams = new URLSearchParams(queryString);
 
 export let dir: 'ltr' | 'rtl' =
     (urlParams.get('sp_dir') as 'ltr' | 'rtl') || 'ltr';
+export let theme: Flavor = (urlParams.get('sp_theme') as Flavor) || 'classic';
 export let color: Color = (urlParams.get('sp_color') as Color) || 'light';
 export let scale: Scale = (urlParams.get('sp_scale') as Scale) || 'medium';
 export let reduceMotion = urlParams.get('sp_reduceMotion') === 'true';
 
 window.__swc_hack_knobs__ = window.__swc_hack_knobs__ || {
+    defaultFlavor: theme,
     defaultColor: color,
     defaultScale: scale,
     defaultDirection: dir,
@@ -71,7 +74,7 @@ const reduceMotionProperties = css`
 ActiveOverlay.prototype.renderTheme = function (
     content: TemplateResult
 ): TemplateResult {
-    const { color, scale, lang } = this.theme;
+    const { color, scale, theme, lang } = this.theme;
     return html`
         ${window.__swc_hack_knobs__.defaultReduceMotion
             ? html`
@@ -83,6 +86,7 @@ ActiveOverlay.prototype.renderTheme = function (
               `
             : html``}
         <sp-theme
+            theme=${ifDefined(theme)}
             color=${ifDefined(color)}
             scale=${ifDefined(scale)}
             lang=${ifDefined(lang)}
@@ -94,76 +98,83 @@ ActiveOverlay.prototype.renderTheme = function (
 };
 
 export class StoryDecorator extends SpectrumElement {
-    static styles = [
-        css`
-            :host(:focus) {
-                outline: none;
-            }
-            sp-theme {
-                overflow-x: hidden;
-                display: block;
-                box-sizing: border-box;
-                width: 100%;
-                min-height: 100vh;
-                padding: var(--spectrum-global-dimension-size-100)
-                    var(--spectrum-global-dimension-size-100)
-                    calc(
-                        2 * var(--spectrum-alias-focus-ring-size) +
-                            var(--spectrum-alias-item-height-m)
+    static get styles() {
+        return [
+            css`
+                :host(:focus) {
+                    outline: none;
+                }
+                sp-theme {
+                    overflow-x: hidden;
+                    display: block;
+                    box-sizing: border-box;
+                    width: 100%;
+                    min-height: 100vh;
+                    padding: var(--spectrum-global-dimension-size-100)
+                        var(--spectrum-global-dimension-size-100)
+                        calc(
+                            2 * var(--spectrum-alias-focus-ring-size) +
+                                var(--spectrum-alias-item-height-m)
+                        );
+                    box-sizing: border-box;
+                    background-color: var(--spectrum-global-color-gray-100);
+                    color: var(
+                        --spectrum-body-text-color,
+                        var(--spectrum-alias-text-color)
                     );
-                background-color: var(--spectrum-global-color-gray-100);
-                color: var(
-                    --spectrum-body-text-color,
-                    var(--spectrum-alias-text-color)
-                );
-            }
-            :host([screenshot]) sp-theme {
-                padding: var(--spectrum-global-dimension-size-100);
-                --swc-test-caret-color: transparent;
-            }
-            :host([reduce-motion]) sp-theme {
-                ${reduceMotionProperties}
-            }
-            .manage-theme {
-                position: fixed;
-                bottom: 0;
-                left: var(--spectrum-global-dimension-size-200);
-                right: var(--spectrum-global-dimension-size-200);
-                display: flex;
-                align-items: center;
-                justify-content: flex-end;
-                box-sizing: border-box;
-                background-color: var(--spectrum-global-color-gray-100);
-                padding-bottom: calc(2 * var(--spectrum-alias-focus-ring-size));
-            }
-            [dir='ltr'] sp-field-label {
-                padding-left: 0;
-                padding-right: var(
-                    --spectrum-fieldlabel-side-padding-x,
-                    var(--spectrum-global-dimension-size-100)
-                );
-                margin-left: var(--spectrum-global-dimension-size-400);
-            }
-            [dir='ltr'] sp-switch {
-                margin-left: var(--spectrum-global-dimension-size-400);
-                margin-right: 0;
-                padding: 0;
-            }
-            [dir='rtl'] sp-field-label {
-                padding-right: 0;
-                padding-left: var(
-                    --spectrum-fieldlabel-side-padding-x,
-                    var(--spectrum-global-dimension-size-100)
-                );
-                margin-right: var(--spectrum-global-dimension-size-400);
-            }
-            [dir='rtl'] sp-switch {
-                margin-right: var(--spectrum-global-dimension-size-400);
-                margin-left: 0;
-                padding: 0;
-            }
-        `,
-    ];
+                }
+                :host([screenshot]) sp-theme {
+                    padding: var(--spectrum-global-dimension-size-100);
+                }
+                :host([reduce-motion]) sp-theme {
+                    ${reduceMotionProperties}
+                }
+                .manage-theme {
+                    position: fixed;
+                    bottom: 0;
+                    left: var(--spectrum-global-dimension-size-200);
+                    right: var(--spectrum-global-dimension-size-200);
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-end;
+                    box-sizing: border-box;
+                    background-color: var(--spectrum-global-color-gray-100);
+                    padding-bottom: calc(
+                        2 * var(--spectrum-alias-focus-ring-size)
+                    );
+                }
+                [dir='ltr'] sp-field-label {
+                    padding-left: 0;
+                    padding-right: var(
+                        --spectrum-fieldlabel-side-padding-x,
+                        var(--spectrum-global-dimension-size-100)
+                    );
+                    margin-left: var(--spectrum-global-dimension-size-400);
+                }
+                [dir='ltr'] sp-switch {
+                    margin-left: var(--spectrum-global-dimension-size-400);
+                    margin-right: 0;
+                    padding: 0;
+                }
+                [dir='rtl'] sp-field-label {
+                    padding-right: 0;
+                    padding-left: var(
+                        --spectrum-fieldlabel-side-padding-x,
+                        var(--spectrum-global-dimension-size-100)
+                    );
+                    margin-right: var(--spectrum-global-dimension-size-400);
+                }
+                [dir='rtl'] sp-switch {
+                    margin-right: var(--spectrum-global-dimension-size-400);
+                    margin-left: 0;
+                    padding: 0;
+                }
+            `,
+        ];
+    }
+
+    @property({ type: String })
+    public theme: Flavor = window.__swc_hack_knobs__.defaultFlavor;
 
     @property({ type: String })
     public color: Color = window.__swc_hack_knobs__.defaultColor;
@@ -182,16 +193,16 @@ export class StoryDecorator extends SpectrumElement {
     public screenshot = false;
 
     @queryAsync('sp-theme')
-    private theme!: Theme;
+    private themeRoot!: Theme;
 
     public ready = false;
 
     public async startManagingContentDirection(el: HTMLElement): Promise<void> {
-        (await this.theme).startManagingContentDirection(el);
+        (await this.themeRoot).startManagingContentDirection(el);
     }
 
     public async stopManagingContentDirection(el: HTMLElement): Promise<void> {
-        (await this.theme).stopManagingContentDirection(el);
+        (await this.themeRoot).stopManagingContentDirection(el);
     }
 
     private updateTheme({ target }: Event & { target: Picker | Switch }): void {
@@ -199,6 +210,12 @@ export class StoryDecorator extends SpectrumElement {
         const { value } = target as Picker;
         const { checked } = target as Switch;
         switch (id) {
+            case 'theme':
+                this.theme =
+                    theme =
+                    window.__swc_hack_knobs__.defaultFlavor =
+                        value as Flavor;
+                break;
             case 'color':
                 this.color =
                     color =
@@ -234,11 +251,22 @@ export class StoryDecorator extends SpectrumElement {
         }
     }
 
+    private get themedColor(): Color {
+        return (this.color +
+            (this.theme === 'express' ? '-express' : '')) as Color;
+    }
+
+    private get themedScale(): Scale {
+        return (this.scale +
+            (this.theme === 'express' ? '-express' : '')) as Scale;
+    }
+
     protected render(): TemplateResult {
         return html`
             <sp-theme
-                color=${this.color}
-                scale=${this.scale}
+                theme=${this.theme}
+                color=${this.themedColor}
+                scale=${this.themedScale}
                 dir=${this.direction}
                 part="container"
                 @keydown=${this.handleKeydown}
@@ -274,9 +302,25 @@ export class StoryDecorator extends SpectrumElement {
     private get manageTheme(): TemplateResult {
         return html`
             <div class="manage-theme" part="controls">
-                ${this.colorControl} ${this.scaleControl} ${this.dirControl}
-                ${this.reduceMotionControl}
+                ${this.themeControl} ${this.colorControl} ${this.scaleControl}
+                ${this.dirControl} ${this.reduceMotionControl}
             </div>
+        `;
+    }
+
+    private get themeControl(): TemplateResult {
+        return html`
+            <sp-field-label for="theme">Spectrum</sp-field-label>
+            <sp-picker
+                id="theme"
+                placement="top"
+                quiet
+                .value=${this.theme}
+                @change=${this.updateTheme}
+            >
+                <sp-menu-item value="classic">Classic</sp-menu-item>
+                <sp-menu-item value="express">Express</sp-menu-item>
+            </sp-picker>
         `;
     }
 